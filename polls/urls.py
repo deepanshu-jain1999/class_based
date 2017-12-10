4a6d63a5d52e610b7ddf8560982b1540c80af904
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2017-12-10 18:57:51 +0530

    Add login and signup

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
-from .views import Signup, Home
+from .views import Signup, Home, Login
+from django.contrib.auth import views as built_views
+
 
 urlpatterns = [
     url(r'^signup$', Signup.as_view(), name='signup'),
+    url(r'^login$', Login.as_view(), name='login'),
     url(r'^home$', Home.as_view(), name='home'),
+    url(r'^logout$', built_views.logout, {'next_page': 'login'}, name='logout'),
+
 ]
 
 
--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render, redirect
 from .forms import SignupForm
 from .models import User
 from django.views.generic import ListView
+from django.contrib.auth.views import login
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class Signup(ListView):
     template_name = 'polls/signup.html'
     form_class = SignupForm
     model = User
+
+    # dispatch override get and post function
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super(Signup, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         form = self.form_class
@@ -16,18 +25,40 @@ class Signup(ListView):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            form.save()
+            email = form.cleaned_data.get('email')
+            if User.objects.filter(email=email).exists():
+                return render(request, self.template_name, {'form': form})
+            else:
+                form.save()
             return redirect('home')
         else:
             form = self.form_class
         return render(request, self.template_name,{'form': form})
 
 
+class Login(ListView):
+    template_name = 'polls/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super(Login, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return login(request, template_name=self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        return login(request, template_name=self.template_name)
+
+
+@method_decorator(login_required, name='get')
 class Home(ListView):
     template_name = 'polls/home.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, 'polls/home.html')
+
+
 
 
 
